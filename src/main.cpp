6cfce39878c4c3813763cda7debbe9ebad4b4b3c
Marko V <markoaurelije@gmail.com>
2023-02-06 16:44:24 +0100

    refactored

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <main.h>
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
@@ -10,61 +11,84 @@
 
 #define MOTION_PIN 0
 #define MOTION_PIN_LED 2
-#define RX_LED_PIN 15
-
-// Variable to store if sending data was successful
-String success;
+#define LED1_PIN 15
+#define LED2_PIN 13
 
 // receiver mac 5C:CF:7F:11:3A:B2
 // sender mac 5C:CF:7F:0B:94:F9
-u8 broadcastAddress[] = {0x5c, 0xCf, 0x7F, 0x11, 0x3A, 0xB2};
+#ifdef SENDER_VARIANT
+u8 destionationAddress[] = {0x5c, 0xCf, 0x7F, 0x11, 0x3A, 0xB2};
+#else
+#endif
+u8 new_data = 0;
 
 // Structure example to send data
 // Must match the receiver structure
 typedef struct struct_message
 {
-  float temp;
-  float hum;
-  int motionDetected;
+  unsigned int counter1;
+  unsigned int counter2;
+  int motion;
+  uint8_t deviceMAC[6];
+
+  String toString()
+  {
+    String result = "";
+    result += "MAC: ";
+    result += MAC_toString(deviceMAC);
+    result += "\r\ncounter1: ";
+    result += String(counter1);
+    result += "\r\ncounter2: ";
+    result += String(counter2);
+    result += "\r\nmotion: ";
+    result += String(motion);
+    return result;
+  }
 } struct_message;
 
 struct_message data_to_send;
 struct_message incomingReadings;
 
+unsigned int unsuccesfull_tx = 0;
+unsigned int unsuccesfull_ack = 0;
+
 void OnDataSent(unsigned char *mac_addr, unsigned char status)
 {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == 0 ? "Delivery Success" : "Delivery Fail");
-  if (status == 0)
+  if (status != 0)
+    unsuccesfull_ack++;
+}
+
+String MAC_toString(uint8_t *mac)
+{
+  String result = "";
+  for (int i = 0; i < 6; ++i)
   {
-    success = "Delivery Success :)";
+    result += String(mac[i], 16);
+    if (i < 5)
+    {
+      result += ':';
+    }
   }
-  else
-  {
-    success = "Delivery Fail :(";
-  }
+  return result;
 }
 
 void OnDataRecv(u8 *mac, u8 *incomingData, u8 len)
 {
-  digitalWrite(RX_LED_PIN, HIGH);
-  Serial.print("Bytes received: ");
-  Serial.println(len);
+  new_data = len;
   memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
-
-  Serial.print("temperature");
-  Serial.println(incomingReadings.temp);
-  Serial.print("humidity");
-  Serial.println(incomingReadings.hum);
-  Serial.print("Motion");
-  Serial.println(incomingReadings.motionDetected);
-  digitalWrite(MOTION_PIN_LED, incomingReadings.motionDetected > 0 ? LOW : HIGH);
 }
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Starting");
+  Serial.print("Starting as");
+#ifdef SENDER_VARIANT
+  Serial.println(" sender");
+#else
+  Serial.println(" receiver");
+#endif
   WiFi.mode(WIFI_STA);
   Serial.print("MAC: ");
   Serial.println(WiFi.macAddress());
@@ -83,31 +107,42 @@ void setup()
   esp_now_register_send_cb(OnDataSent);
 
   // Add peer
-  if (esp_now_add_peer(broadcastAddress, esp_now_role::ESP_NOW_ROLE_SLAVE, 0, NULL, 0) != 0)
+  if (esp_now_add_peer(destionationAddress, esp_now_role::ESP_NOW_ROLE_SLAVE, 0, NULL, 0) != 0)
   {
     Serial.println("Failed to add peer");
     return;
   }
   pinMode(MOTION_PIN, INPUT_PULLUP); // GPIO0 btn
-  data_to_send.temp = 25.5;
-  data_to_send.hum = 50.5;
+  data_to_send.counter1 = unsuccesfull_ack;
+  data_to_send.counter2 = unsuccesfull_tx;
+  WiFi.macAddress(data_to_send.deviceMAC);
+  Serial.print("Sender mac: ");
+  Serial.println(MAC_toString(data_to_send.deviceMAC));
 #else
   pinMode(MOTION_PIN_LED, OUTPUT);
-  pinMode(RX_LED_PIN, OUTPUT);
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
   // Register for a callback function that will be called when data is received
   esp_now_register_recv_cb(OnDataRecv);
 #endif
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
 }
 
 int cnt = 0;
 void loop()
 {
 #ifdef SENDER_VARIANT
-  data_to_send.motionDetected = digitalRead(MOTION_PIN);
-  int result = esp_now_send(broadcastAddress, (u8 *)&data_to_send, sizeof(data_to_send));
+  Serial.print("loop");
+  Serial.println(cnt++);
 
-  data_to_send.temp += 0.1;
+  u8 success = data_to_send.counter1 == unsuccesfull_ack ? 0 : 1;
+
+  data_to_send.motion = digitalRead(MOTION_PIN);
+  data_to_send.counter1 = unsuccesfull_ack;
+  data_to_send.counter2 = unsuccesfull_tx;
+  int result = esp_now_send(destionationAddress, (u8 *)&data_to_send, sizeof(data_to_send));
+  Serial.print("Sending data: ");
+  Serial.println(data_to_send.toString());
 
   if (result == 0)
   {
@@ -115,13 +150,31 @@ void loop()
   }
   else
   {
+    unsuccesfull_tx++;
+    success++;
     Serial.println("Error sending the data");
   }
+  if (success != 0)
+  {
+    digitalWrite(LED1_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED1_PIN, LOW);
+    delay(900);
+  }
+  else
+    delay(1000);
 #else
-  digitalWrite(RX_LED_PIN, LOW);
-#endif
 
-  Serial.print("loop");
-  Serial.println(cnt++);
-  delay(1000);
+  if (new_data > 0)
+  {
+    digitalWrite(LED1_PIN, HIGH);
+    Serial.print("Bytes received: ");
+    Serial.println(new_data);
+    Serial.println(incomingReadings.toString());
+    digitalWrite(MOTION_PIN_LED, incomingReadings.motion > 0 ? LOW : HIGH);
+    new_data = 0;
+    delay(100);
+    digitalWrite(LED1_PIN, LOW);
+  }
+#endif
 }
---
+++ src/main.h
@@ -0,0 +1,2 @@
+#include <Arduino.h>
+String MAC_toString(uint8_t *mac);
